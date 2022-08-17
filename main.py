e453b98118229e52964f0a532ee77ef911a5f102
Alisson <alissondvorak@gmail.com>
2022-08-17 15:13:48 -0300

    Subindo Projeto

---
+++ main.py
@@ -0,0 +1,79 @@
+
+import os, io
+
+def read_files(dir, ftype="txt"):
+    import fitz
+
+    prev_dir = os.getcwd()
+    print(f"changing cwd to {dir}")
+    os.chdir(dir) 
+    print(f"reading files...")
+    file_list = [ file_name for file_name in os.listdir(dir) 
+                    if file_name[-len(ftype):] == ftype ]
+    for file_name in file_list:
+        #TODO : remove slicing after devel
+        print(f"reading {file_name}")
+        yield fitz.open(os.path.join(file_name))
+    os.chdir(prev_dir)
+
+def read_images_from_pdf(files, out_dir):
+    from PIL import Image
+
+    for pdf in files:
+        file_name = os.path.splitext(pdf.name)[0]
+        images = sum([ pdf[i].get_images() for i in range(len(pdf)) ], [])
+        print(f"{len(images)} images in file : {file_name}")
+
+        page_out_dir = os.path.join(out_dir, file_name)
+        os.makedirs(page_out_dir)
+        for n, image in enumerate(images):
+            xref = image[0]
+            
+            extract = pdf.extract_image(xref)
+            image = Image.open(io.BytesIO(extract["image"]))
+            image.save(open(f"{page_out_dir}/img_{n}.{extract['ext']}", "wb"))
+
+
+
+def read_text_from_pdf(files):
+
+    for pdf in files:
+
+        file_name = os.path.splitext(pdf.name)[0]
+        out_file = os.path.join(out_dir, file_name)
+        text = str()
+
+        for page in pdf: text += page.get_text()
+        print(text)
+
+        with open(f"{out_file}.txt", "w", encoding='utf-8') as f:
+            f.write(text)
+
+
+
+if __name__=="__main__":
+    pdf_dir = os.path.join(os.getcwd(), "pdf")
+    out_dir = os.path.join(os.getcwd(), "out")
+    files = [ pdf for pdf in read_files(dir=pdf_dir, ftype="pdf") ]
+
+
+    while True:
+        print("""
+                 Digite a funcao que deseja realizar:
+                 (0) = Sair
+                 (1) = Extrair Imagens
+                 (2) = Extrair Textos
+          
+        """)
+        choice = int (input ("Qual a função desejada? "))
+        if choice == 0:
+            print("ENCERRANDO PROGRAMA")
+            break
+        elif choice == 1:
+            read_images_from_pdf(files, out_dir),
+        elif choice == 2:
+            read_text_from_pdf(files)
+        
+    
+
+    
---
+++ teste.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+import PyPDF2
+import re
+
+# Abre o arquivo pdf 
+# lembre-se que para o windows você deve usar essa barra -> / 
+# lembre-se também que você precisa colocar o caminho absoluto
+pdf_file = open('pdf/ita.pdf', 'rb')
+
+#Faz a leitura usando a biblioteca
+read_pdf = PyPDF2.PdfFileReader(pdf_file)
+# pega o numero de páginas
+number_of_pages = read_pdf.getNumPages()
+#lê a primeira página completa
+page = read_pdf.getPage(0)
+
+#extrai apenas o texto
+page_content = page.extractText()
+print("--------------------------------------------------------------------------")
+print(page_content)
+print("--------------------------------------------------------------------------")
+teste = page_content.encode('utf8')
+print(teste)
+
+parsed = ''.join(page_content)
+
+print("Sem eliminar as quebras")
+print(parsed)
+
+# remove as quebras de linha
+parsed = re.sub('n', '', parsed)
+print("Após eliminar as quebras")
+print(parsed)
+
+print("nPegando apenas as 20 primeiras posições")
+novastring = parsed[0:20]
+print(novastring)
